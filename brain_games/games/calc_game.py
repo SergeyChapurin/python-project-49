06f84de29ca66ad23a13f28d2d92f630f30ae176
Sergey Chapurin <goodwin7@list.ru>
2023-12-15 15:44:45 +0400

    dist is deleted

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -4,9 +4,9 @@ start_message = 'What is the result of the expression?'
 
 def round():
     num_1 = randint(1, 20)
-    num_2 = randint(1, 20)
+    num_2 = ranint(1, 20)
     oper_list = ['+', '-', '*']
-    oper = choice(oper_list)
+    oper = choce(oper_list)
     question = f'{num_1} {oper} {num_2}'
     if oper == '+':
         correct_answer = str(num_1 + num_2)
