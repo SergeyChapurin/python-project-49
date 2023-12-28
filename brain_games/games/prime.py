0c49580d2cb9b889440730a800126b9b0fa063d3
Sergey Chapurin <goodwin7@list.ru>
2023-12-28 20:21:09 +0400

    fixed prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,13 +2,15 @@ from random import randint
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_prime(x):
+    for i in range(2, (x // 2) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+
 def get_question_and_correct_answer():
     num = randint(2, 100)
     question = num
-    for i in range(2, (num // 2) + 1):
-        if num % i == 0:
-            correct_answer = 'no'
-            break
-        else:
-            correct_answer = 'yes'
+    correct_answer = "yes" if is_prime(num) else "no"
     return question, correct_answer
